4b4322a78b10655e80555e9782b7310122c2472e
Ilya Bashlyaev <87471648+IlyaBashlyaev@users.noreply.github.com>
2022-08-03 20:00:11 +0200

    Main Commit

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, {'width': 12, 'height': 20}),
+    path('', views.index, {'width': 12, 'height': 20}),
     path('settings', views.settings),
     path('width<int:width>/height<int:height>', views.changeSize)
 ]
