bb7494b6159875c600a378fbc64498ac4777b8f8
Ilya Bashlyaev <87471648+IlyaBashlyaev@users.noreply.github.com>
2022-08-03 19:58:14 +0200

    Main Commit

--- tetris/urls.py
+++ tetris/urls.py
@@ -5,7 +5,7 @@ from django.views.static import serve
 from django.conf.urls import url
 
 urlpatterns = [
-    path('', include('main.urls')),
+    path('/', include('main.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
